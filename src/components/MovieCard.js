9ba2d8dd82f37f7b0617fa965368f212bb2978e3
Szymon Gos <szymonngos@gmail.com>
2021-09-03 15:00:56 +0100

    add movieCard component + styles

---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MovieCard() {
+    return (
+        <div className='card'>
+            <div className="card__poster-img"></div>
+            <div className="card__body">
+                    <h3 className="card__title">The Best Movie Title In The World</h3>
+            </div>
+        </div>
+    )
+}
