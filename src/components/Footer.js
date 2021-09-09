8ff83c1fe0a067151f2f07f151fb203966584ef9
Szymon Gos <szymonngos@gmail.com>
2021-09-09 17:27:10 +0100

    add newsletter to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,22 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className='footer'>
+            <div className="newsletter">
+                <div className='newsletter__content'>
+                    <div className="newsletter__text">
+                        <h2 className="newsletter__title">
+                            Subscribe to our Newsletter!
+                        </h2>
+                        <p>Receive the latest movie <strong>recommendations</strong>, <strong>reviews</strong> and <strong>movie news</strong> every two weeks.</p>
+                    </div>
+                    <div className="newsletter__input-group">
+                        <form className="newsletter__form">
+                            <input className='newsletter__input' type="text" name="email" placeholder='Enter email address' />
+                            <button class='button' type='submit' onClick={(e) => e.preventDefault()}>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
             <div className="footer-content">
                 <div className="credits">
                     <span>Powered by </span><a href="https://www.themoviedb.org/" target="_blank">TMDb</a>
